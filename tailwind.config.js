538153ab9e3d4eff7935c492572dff88fe6070f1
Lucas Del <deleportelucas44@gmail.com>
2024-04-03 16:33:18 +0200

    avancement page d'accueil mais probleme de marges et de texte

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3BAC76",
+      },
+      fontFamily: {
+        romantica: ["Romantica", "sans-serif"],
+        genesys: ["Genesys", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
+
+// npx tailwindcss -i ./css/style.css -o ./src/style.css --watch
